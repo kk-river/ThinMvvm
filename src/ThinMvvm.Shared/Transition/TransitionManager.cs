ada2ad7150ded1b73266bb3461f5474a4b1d1def
kk-river <13160818+kk-river@users.noreply.github.com>
2023-12-18 07:50:08 +0900

    :bug: Fix crash on TransitionManager.Frames

--- src/ThinMvvm.Shared/Transition/TransitionManager.cs
+++ src/ThinMvvm.Shared/Transition/TransitionManager.cs
@@ -13,15 +13,15 @@ public class TransitionManager : ITransitionManager
     #region Dependency
     public static readonly DependencyProperty FrameNameProperty = DependencyProperty.RegisterAttached("FrameName", typeof(string), typeof(TransitionManager), new PropertyMetadata(defaultValue: null, OnFrameNameChanged));
 
-    private static void OnFrameNameChanged(DependencyObject element, DependencyPropertyChangedEventArgs args) => s_transitionFrames.Add((string)args.NewValue, new((FrameworkElement)element));
+    private static void OnFrameNameChanged(DependencyObject element, DependencyPropertyChangedEventArgs args) => s_frames.Add((string)args.NewValue, new Frame((FrameworkElement)element));
 
     public static void SetFrameName(DependencyObject regionTarget, string regionName) => regionTarget.SetValue(FrameNameProperty, regionName);
 
     public static string GetFrameName(DependencyObject regionTarget) => (string)regionTarget.GetValue(FrameNameProperty);
     #endregion Dependency
 
-    internal static readonly Dictionary<string, Frame> s_transitionFrames = [];
-    public IReadOnlyDictionary<string, IFrame> Frames => (IReadOnlyDictionary<string, IFrame>)s_transitionFrames;
+    internal static readonly Dictionary<string, IFrame> s_frames = [];
+    public IReadOnlyDictionary<string, IFrame> Frames => s_frames;
 
     private readonly IServiceProvider _provider;
 
@@ -32,13 +32,13 @@ public class TransitionManager : ITransitionManager
 
     public void RequestTransit(string frameName, string viewName)
     {
-        if (!s_transitionFrames.TryGetValue(frameName, out Frame? frame))
+        if (!s_frames.TryGetValue(frameName, out IFrame? frame))
         {
             throw new KeyNotFoundException($"Frame '{frameName}' not found.");
         }
 
         ViewRegistration view = _provider.GetRequiredKeyedService<ViewRegistration>(viewName);
-        frame.PasteView(view);
+        ((Frame)frame).PasteView(view);
     }
 
     public void ShowWindow(string windowName)
